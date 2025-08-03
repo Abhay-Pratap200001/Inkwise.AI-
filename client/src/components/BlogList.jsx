e1710cdb63df13b519dfd1a255aaac8cb450ba39
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-08-04 00:18:42 +0530

    Complete the blog list page for lsiting the blog according to categories

---
+++ client/src/components/BlogList.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { motion } from "motion/react";
+import { blog_data, blogCategories } from "../assets/assets";
+import { useState } from "react";
+import BlogCard from "./BlogCard";
+
+function BlogList() {
+  const [menu, setMenu] = useState("All");
+
+  return (
+    <div>
+      <div className="flex justify-center gap-4 sm:gap-8 my-10 relative">
+        {blogCategories.map((items) => (
+          <div key={items} className="realtive inline-block">
+            <button
+              onClick={() => setMenu(items)}
+              className={` relative cursor-pointer text-gray-600 ${
+              menu === items && "text-gray-800 px-4 pt-0.5"}`}>{/*comparing types of button*/}
+              {items}
+              {menu === items && (<motion.div 
+              layoutId="underline"
+              transition={{ type: "spring", stiffness: 500, damping: 10 }}
+              className="absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full">
+              </motion.div>)}
+            </button>
+          </div>
+        ))}
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40">
+        {blog_data.filter((blog) => (menu === "All" ? true : blog.category === menu)).map((blog, index) => (
+            <BlogCard key={index} blog={{...blog, _id:index}} />
+          ))}
+        {/*sending bllogs according to the needs*/}
+      </div>
+    </div>
+  );
+}
+
+export default BlogList;
