4ab6dd3d37bb84570e59af86c79822dca240f652
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-08-11 19:55:42 +0530

    Middleware for protected routes

---
+++ server/middleware/auth.js
@@ -0,0 +1,10 @@
+import jwt from 'jsonwebtoken';
+
+export const auth = (req, res, next) => {  
+  const token = req.headers.authorization;  
+try {
+      jwt.verify(token, process.env.JWT_SECRET);
+      next();
+} catch (error) {
+    res.json({success: false, message: 'Inavlid token'})
+}};
