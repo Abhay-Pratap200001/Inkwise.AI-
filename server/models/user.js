a9e0538fc0c8f833c8a5c8ebe122f5dea03c6d73
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-08-11 19:57:35 +0530

    User model

---
+++ server/models/user.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+//Making userschema
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema); //making usermodel
+
+export default User;
